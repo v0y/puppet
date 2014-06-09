c968f51c3215e429555507eb41437b1223d51882
Rafał Mirończyk <voyageur.pl@gmail.com>
2014-06-09 21:56:43 +0200

    add bind module - dns configuration for lolwtf.pl

---
+++ modules/bind/files/etc/bind.applejack/db.lolwtf.pl
@@ -0,0 +1,19 @@
+$TTL 1800s
+$ORIGIN lolwtf.pl.
+
+@  IN  SOA NS1.DIGITALOCEAN.COM.  hostmaster.lolwtf.pl. (
+  2014052600  ; serial
+  3600        ; refreshs
+  900         ; refresh update retry
+  1209600     ; when master dies.. after this time slave goes away..
+  1800        ; the time a name error = nx domain may be cached by any resolver
+)
+
+@  IN  NS  NS1.DIGITALOCEAN.COM.
+@  IN  NS  NS2.DIGITALOCEAN.COM.
+@  IN  NS  NS3.DIGITALOCEAN.COM.
+
+@  300 IN  A 188.226.222.101
+@  300 IN TXT "v=spf1 a:lolwtf.pl ~all"
+
+*  188.226.222.101
---
+++ modules/bind/manifests/init.pp
@@ -0,0 +1,30 @@
+class bind {
+  package {
+    [ 'bind9' ]:
+      ensure => installed
+  }
+
+  service {
+    'bind':
+      name => 'bind9',
+      ensure => running,
+      restart => 'invoke-rc.d bind9 reload',
+      require => [
+        Package['bind9'],
+        File['/etc/bind']
+      ]
+  }
+
+  file {
+    '/etc/bind':
+      ensure => directory,
+      recurse => true,
+      group => 'bind',
+      source  => [
+        "puppet:///modules/bind/etc/bind.${::hostname}",
+        "puppet:///modules/bind/etc/bind.${::serverkind}",
+        'puppet:///modules/bind/etc/bind/'
+      ],
+      notify => Service['bind']
+  }
+}
