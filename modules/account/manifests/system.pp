39803081fb941a821de6badd11df63c82b40c1bf
Rafał Mirończyk <voyageur.pl@gmail.com>
2014-07-06 21:07:56 +0200

    add system.pp manifest in account module

---
+++ modules/account/manifests/system.pp
@@ -0,0 +1,33 @@
+define account::system(
+  $uid,
+  $gid,
+  $home,
+  $home_mode,
+  $shell,
+  $groups
+) {
+  group {
+    $title:
+      ensure => present,
+      gid    => $gid
+  }
+
+  user {
+    $title:
+      ensure  => present,
+      uid     => $uid,
+      gid     => $gid,
+      shell   => $shell,
+      home    => $home,
+      groups  => $groups,
+      require => Group[$title]
+  }
+
+  file {
+    $home:
+      ensure  => directory,
+      owner   => $title,
+      group   => $title,
+      mode    => $home_mode,
+      require => User[$title]
+  }
