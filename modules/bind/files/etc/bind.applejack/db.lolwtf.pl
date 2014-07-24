5ddd7e661b3afb9614c48271cc9910643579ca94
Rafał Mirończyk <voyageur.pl@gmail.com>
2014-07-24 23:46:37 +0200

    add MX records for zohomail

--- modules/bind/files/etc/bind.applejack/db.lolwtf.pl
+++ modules/bind/files/etc/bind.applejack/db.lolwtf.pl
@@ -2,7 +2,7 @@ $TTL 1800s
 $ORIGIN lolwtf.pl.
 
 @  IN  SOA NS1.DIGITALOCEAN.COM.  hostmaster.lolwtf.pl. (
-  2014052600  ; serial
+  1406236964  ; last update: 2014-07-24 21:22:43 UTC
   3600        ; refreshs
   900         ; refresh update retry
   1209600     ; when master dies.. after this time slave goes away..
@@ -12,6 +12,9 @@ $ORIGIN lolwtf.pl.
 @  IN  NS  NS1.DIGITALOCEAN.COM.
 @  IN  NS  NS2.DIGITALOCEAN.COM.
 @  IN  NS  NS3.DIGITALOCEAN.COM.
+
+MX 10 mx.zohomail.com.
+MX 20 mx2.zohomail.com.
 
 @  300 IN  A 188.226.222.101
 @  300 IN TXT "v=spf1 a:lolwtf.pl ~all"
