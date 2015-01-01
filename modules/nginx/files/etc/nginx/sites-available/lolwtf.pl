f8ce529c553542e68d5466ad786ffdf194fabe6b
Rafał Mirończyk <voyageur.pl@gmail.com>
2015-01-01 21:29:12 +0100

    root maybe?

--- modules/nginx/files/etc/nginx/sites-available/lolwtf.pl
+++ modules/nginx/files/etc/nginx/sites-available/lolwtf.pl
@@ -15,12 +15,12 @@ server {
     rewrite ^ http://lolwtf.pl;
   }
 
-  location /static/ {
-    alias /var/www/lolwtf.pl/public/static/;
+  location /static {
+    root /var/www/lolwtf.pl/public;
   }
 
-  location /media/ {
-    alias /var/www/lolwtf.pl/public/media/;
+  location /media {
+    root /var/www/lolwtf.pl/public;
   }
 
   location / {
