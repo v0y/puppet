e48685b7685220ebb1ecc084262675498f311fc2
Rafał Mirończyk <voy@lolwtf.pl>
2015-05-12 01:38:55 +0200

    add creating venv

--- modules/virtualenv/manifests/config.pp
+++ modules/virtualenv/manifests/config.pp
@@ -11,10 +11,10 @@ define virtualenv::config(
     group    => 'virtualenv'
   }
 
-  file { "/var/lib/virtualenv/${title}/production":
-    ensure   => directory,
-    owner    => $title,
-    mode     => '0664',
-    group    => 'virtualenv'
+  python::pyvenv { "/var/lib/virtualenv/${title}/production" :
+    ensure       => present,
+    version      => 'system',
+    owner        => $title,
+    group        => 'virtualenv',
   }
 }
