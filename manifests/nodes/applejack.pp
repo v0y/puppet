9e17dd2912de85a0b7b2a571e4f679c90fe2a4ae
Rafał Mirończyk <voy@lolwtf.pl>
2015-05-17 22:44:54 +0200

    add polski bus settings

--- manifests/nodes/applejack.pp
+++ manifests/nodes/applejack.pp
@@ -10,6 +10,7 @@ node 'applejack' {
   include virtual::account::system
   Account::System <| title == 'deployer' |>
   Account::System <| title == 'lolwtf.pl' |>
+  Account::System <| title == 'polski-bus.lolwtf.pl' |>
 
   include virtual::account::human
   Account::Human <| title == 'voy' |>
@@ -21,34 +22,48 @@ node 'applejack' {
         Uwsgi::Config['lolwtf.pl']
       ]
   }
+  nginx::vhost::config {
+    [ 'polski-bus.lolwtf.pl' ]:
+      require => [
+        Uwsgi::Config['polski-bus.lolwtf.pl']
+      ]
+  }
 
   include uwsgi
   uwsgi::config {
     [ 'lolwtf.pl' ]:
       ensure => present
   }
-
-  include python
-  class { 'python' :
-    version    => '3',
-    pip        => true,
-    dev        => true,
-    virtualenv => true,
-    gunicorn   => false,
+  uwsgi::config {
+    [ 'polski-bus.lolwtf.pl' ]:
+      ensure => present
   }
 
-  python::pyvenv { '/var/www/lolwtf.pl' :
-    ensure       => present,
-    version      => '3',
-    systempkgs   => true,
-    venv_dir     => '/var/lib/virtualenv/lolwtf.pl',
-    owner        => 'lolwtf.pl',
-    group        => 'virtualenv',
-  }
-
-#  include virtualenv
-#  virtualenv::config{
-#    [ 'lolwtf.pl' ]:
-#      ensure => present
+#  include python
+#  class { 'python' :
+#    version    => '3',
+#    pip        => true,
+#    dev        => true,
+#    virtualenv => true,
+#    gunicorn   => false,
 #  }
+
+#  python::pyvenv { '/var/www/lolwtf.pl' :
+#    ensure       => present,
+#    version      => '3',
+#    systempkgs   => true,
+#    venv_dir     => '/var/lib/virtualenv/lolwtf.pl',
+#    owner        => 'lolwtf.pl',
+#    group        => 'virtualenv',
+#  }
+
+  include virtualenv
+  virtualenv::config{
+    [ 'lolwtf.pl' ]:
+      ensure => present
+  }
+  virtualenv::config{
+    [ 'polski-bus.lolwtf.pl' ]:
+      ensure => present
+  }
 }
--- modules/virtual/manifests/account/system.pp
+++ modules/virtual/manifests/account/system.pp
@@ -9,6 +9,15 @@ class virtual::account::system {
       groups    => [ 'ssh', 'users', 'uwsgi', 'virtualenv', 'webroot', 'deployer' ]
   }
   @account::system {
+    'polski-bus.lolwtf.pl':
+      uid       => 5000,
+      gid       => 5000,
+      shell     => '/bin/false',
+      home      => '/var/www/polski-bus.lolwtf.pl',
+      home_mode => '0751',
+      groups    => [ 'ssh', 'users', 'uwsgi', 'virtualenv', 'webroot', 'deployer' ]
+  }
+  @account::system {
     'deployer':
       uid       => 5001,
       gid       => 5001,
