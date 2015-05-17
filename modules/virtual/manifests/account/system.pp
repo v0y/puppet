5890458ef718328a8c76c5b3f07bfbe135b40020
Rafał Mirończyk <voy@lolwtf.pl>
2015-05-17 23:19:28 +0200

    fix gids and uids

--- modules/virtual/manifests/account/system.pp
+++ modules/virtual/manifests/account/system.pp
@@ -9,15 +9,6 @@ class virtual::account::system {
       groups    => [ 'ssh', 'users', 'uwsgi', 'virtualenv', 'webroot', 'deployer' ]
   }
   @account::system {
-    'polski-bus.lolwtf.pl':
-      uid       => 5000,
-      gid       => 5000,
-      shell     => '/bin/false',
-      home      => '/var/www/polski-bus.lolwtf.pl',
-      home_mode => '0751',
-      groups    => [ 'ssh', 'users', 'uwsgi', 'virtualenv', 'webroot', 'deployer' ]
-  }
-  @account::system {
     'deployer':
       uid       => 5001,
       gid       => 5001,
@@ -27,9 +18,18 @@ class virtual::account::system {
       groups    => [ 'ssh', 'users', 'uwsgi', 'webroot', 'virtualenv' ]
   }
   @account::system {
+    'polski-bus.lolwtf.pl':
+      uid       => 5002,
+      gid       => 5002,
+      shell     => '/bin/false',
+      home      => '/var/www/polski-bus.lolwtf.pl',
+      home_mode => '0751',
+      groups    => [ 'ssh', 'users', 'uwsgi', 'virtualenv', 'webroot', 'deployer' ]
+  }
+  @account::system {
     'git':
-      uid       => 5001,
-      gid       => 5001,
+      uid       => 5003,
+      gid       => 5003,
       shell     => '/bin/bash',
       home      => '/home/git',
       home_mode => '0751',
