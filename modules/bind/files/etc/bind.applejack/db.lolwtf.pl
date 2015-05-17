1bbc4b12f895c6e1344a2174f89ae02dd8941ab5
Rafał Mirończyk <voy@lolwtf.pl>
2015-05-17 22:54:54 +0200

    update bind settings

--- modules/bind/files/etc/bind.applejack/db.lolwtf.pl
+++ modules/bind/files/etc/bind.applejack/db.lolwtf.pl
@@ -1,22 +1,15 @@
-$TTL 1800s
 $ORIGIN lolwtf.pl.
-
-@  IN  SOA NS1.DIGITALOCEAN.COM.  hostmaster.lolwtf.pl. (
-  1406236964  ; last update: 2014-07-24 21:22:43 UTC
-  3600        ; refreshs
-  900         ; refresh update retry
-  1209600     ; when master dies.. after this time slave goes away..
-  1800        ; the time a name error = nx domain may be cached by any resolver
-)
-
-@  IN  NS  NS1.DIGITALOCEAN.COM.
-@  IN  NS  NS2.DIGITALOCEAN.COM.
-@  IN  NS  NS3.DIGITALOCEAN.COM.
-
-MX 10 mx.zohomail.com.
-MX 20 mx2.zohomail.com.
-
-@  300 IN  A 188.226.222.101
-@  300 IN TXT "v=spf1 a:lolwtf.pl ~all"
-
-*  188.226.222.101
+$TTL 1800
+lolwtf.pl. IN SOA ns1.digitalocean.com. hostmaster.lolwtf.pl. 1431895997 10800 3600 604800 1800
+lolwtf.pl. 1800 IN A 188.226.222.101
+lolwtf.pl. 1800 IN NS ns1.digitalocean.com.
+lolwtf.pl. 1800 IN NS ns2.digitalocean.com.
+lolwtf.pl. 1800 IN NS ns3.digitalocean.com.
+www.lolwtf.pl. 1800 IN CNAME lolwtf.pl.
+*.lolwtf.pl. 1800 IN CNAME lolwtf.pl.
+puppet.lolwtf.pl. 1800 IN CNAME lolwtf.pl.
+lolwtf.pl. 1800 IN TXT "zoho-verification=zmverify.zoho.com"
+zb14062304.lolwtf.pl. 1800 IN CNAME zmverify.zoho.com.
+lolwtf.pl. 1800 IN MX 10 mx.zohomail.com.
+lolwtf.pl. 1800 IN MX 20 mx2.zohomail.com.
+polski-bus.lolwtf.pl. 1800 IN CNAME lolwtf.pl.
